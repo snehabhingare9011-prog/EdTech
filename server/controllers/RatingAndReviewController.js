175116874453869a0ab2fee29c6954fd657259e4
Sneha Bhingare <snehabhingare9011@gmail.com>
2026-06-22 18:41:30 +0530

    Review and Rating controller added

---
+++ server/controllers/RatingAndReviewController.js
@@ -0,0 +1,165 @@
+const Course=require('../models/course');
+const User=require('../models/user');
+const RatingAndReview=require('../models/ratingAndReview');
+
+
+
+exports.createRating=async (req,res)=>{
+    try{
+
+        const userId=req.user.id;
+        const {rating,review,courseId}=req.body;
+
+        if(!rating||!review||!courseId){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required"
+            })
+        }
+        
+        //check if user already enrolled or not
+        const courseDetails=await Course.findOne({
+            _id:courseId,
+            studentEnrolled:{
+                $elemMatch:{
+                    $eq:userId
+                }
+            }
+        });
+       
+        if(!courseDetails){
+             return res.status(403).json({
+                success:false,
+                message:"You must be enrolled in the course to submit a review"
+            })
+        }
+        
+        //check if student already reviewed the course
+        const alreadyReviewed=await RatingAndReview.findOne(
+            {
+               course:courseId,
+                user:userId
+            }
+        );
+
+        if(alreadyReviewed){
+            return res.status(409).json({
+                success:false,
+                message:"You have already reviewed this course"
+            });
+        }
+        
+        //create review and rating
+        const newReview =await RatingAndReview.create({
+            user:userId,
+            rating,
+            review,
+            course:courseID
+        });
+
+        // Add review to course
+        const updatedCourse=await Course.findbyIdAndUpdate(courseId,
+            {
+                $push:{
+                    ratingAndReview:newReview._id
+                }
+            },
+            {new:true}
+        );
+      
+        //send responce
+        return res.status(200).json({
+            success:true,
+            message:"Review submitted successfully"
+        })
+
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+exports.getAverageRating=async (req,res)=>{
+    try{
+        const {courseId} =req.body;
+
+        if(!courseId){
+            return res.status(400).json({
+                success:false,
+                message:"Course ID is required"
+            })
+        }
+
+        const result=await RatingAndReview.aggregate([
+            {
+                $match: {
+                    course: courseID
+                }     
+            },
+            {
+                $group:{
+                    _id:null,
+                    averageRating: {
+                        $avg:"$rating"
+                    }
+                }
+            }
+        ]);
+
+        if(result.length===0){
+            return res.status(200).json({
+                success:true,
+                message:"No ratings found for this course",
+                averageRating:0
+
+            })
+        }
+
+        return res.status(200).json({
+            success:true,
+            message:"Average rating fetched successfully",
+            averageRating:result[0].averageRating
+        })
+
+
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:err.message
+        })
+    }
+}
+
+exports.getAllRating=async(req,res)=>{
+    try{
+
+         const allReviews = await RatingAndReview.find({}) // sort({ rating:-1 }) ==> Highest ratings come first.
+            .sort({ rating:-1 })
+            .populate({
+                path:"user",
+                select:"firstName lastName email image"
+            })
+            .populate({
+                path:"course",
+                select:"courseName"
+            }).exec();
+
+    
+        return res.status(200).json({
+            success: true,
+            message: "All Reviews fetched successfully",
+            data: allReviews
+        });
+
+        
+
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:err.message
+        })
+
+    }
+}
